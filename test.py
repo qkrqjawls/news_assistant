d059f75986191fd091cb016665d1b4ef2f0cb64a
ca1cu1ating <greg.yunho@sasa.hs.kr>
2025-06-10 02:59:10 +0900

    aaa

---
+++ test.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://news-assistant-app-public-1052035590147.us-central1.run.app"
+# data = {
+#     "my_message": "Hello from Python test client!"
+# }
+
+response = requests.get(url+"/api/issues")
+
+print("Status Code:", response.status_code)
+print("Response:", response.json())
+# print("Status Code:", response.status_code)
+# print("Response text:", response.text)
